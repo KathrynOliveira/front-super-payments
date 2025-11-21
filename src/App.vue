a290ad500d11bd470a347c6c9d57fbebb64b301b
KathrynOliveira <k.olliver18@gmail.com>
2025-11-21 12:15:47 -0300

    component initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import NavigationDrawer from './components/NavigationDrawer.vue'
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <!-- Conteúdo principal -->
+    <div class="flex-1 flex flex-col">
+
+      <!-- Navbar -->
+      <header>
+        <Navbar />
+      </header>
+      <!-- Sidebar -->
+      <NavigationDrawer />
+
+      <!-- Main content -->
+      <!-- <main class="flex-1 bg-gray-50">
+
+      </main> -->
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
