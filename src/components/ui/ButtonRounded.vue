08157c894a57a3d05df30b5e9bc5dabafa603951
KathrynOliveira <k.olliver18@gmail.com>
2025-11-21 16:39:53 -0300

    component button rounded

---
+++ src/components/ui/ButtonRounded.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+    icon: {
+        type: String,
+        required: true,
+    },
+    background: {
+        type: String,
+        default: "#FFFFFF",
+    },
+    color: {
+        type: String,
+        default: "#2A2E33",
+    },
+    borderColor: {
+        type: String,
+        default: "#D9D9D9",
+    },
+});
+</script>
+
+<template>
+    <button class="flex items-center justify-center rounded-full cursor-pointer" :style="{
+        width: '46px',
+        height: '46px',
+        background,
+        border: `1px solid ${borderColor}`,
+        padding: '6px 23px',
+    }">
+    {{ icon }}
+        <img v-if="icon" :src="icon" alt="icon" class="w-[20px] h-[20px]" />
+    </button>
+</template>
