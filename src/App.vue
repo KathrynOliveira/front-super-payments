e097be6a4d449c08ceee7029ad8abbaab534cd19
KathrynOliveira <k.olliver18@gmail.com>
2025-11-21 16:41:49 -0300

    adjust layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import NavigationDrawer from './components/NavigationDrawer.vue'
+import DashboardMain from './components/Dashboard/index.vue'
 </script>
 
 <template>
-  <div class="flex h-screen">
-    <!-- Conteúdo principal -->
-    <div class="flex-1 flex flex-col">
+  <div class="h-screen flex flex-col">
 
-      <!-- Navbar -->
-      <header>
-        <Navbar />
-      </header>
-      <!-- Sidebar -->
+    <!-- Navbar -->
+    <header>
+      <Navbar />
+    </header>
+
+    <!-- Layout principal: Drawer + Main lado a lado -->
+    <div class="flex flex-1">
+
+      <!-- Drawer -->
       <NavigationDrawer />
 
-      <!-- Main content -->
-      <!-- <main class="flex-1 bg-gray-50">
-
-      </main> -->
+      <!-- Main -->
+      <main class="flex-1 flex justify-center">
+        <DashboardMain />
+      </main>
 
     </div>
   </div>
